5f99b4ff586551fa2a776fbe9dfda5078b611853
Rakshya Bhatta <bhattarakshya6@gmail.com>
2022-07-10 19:38:41 +0545

    testing

---
+++ hire/test/dashboard_test.dart
@@ -0,0 +1,16 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+import 'package:hire/main.dart';
+import 'package:hire/normaluser/home.dart';
+import 'package:hire/normaluser/splash.dart';
+import 'package:hire/professionals/dashboard.dart';
+
+void main() {
+  testWidgets("Mainpage Testing", (WidgetTester tester) async {
+    await tester.pumpWidget(GetMaterialApp(home:Splash()));
+    expect(find.byType(AnimatedSplashScreen), findsOneWidget);
+  });
+}
